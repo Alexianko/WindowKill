5e0c61292871983e65c3ef746a66821079f28426
panko_alex <derpycraft2001@gmail.com>
2023-11-23 21:54:56 +0300

    Добавьте файлы проекта.

---
+++ WindowKill/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowKill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.canvas = new System.Windows.Forms.Panel();
+            this.canvas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.TimerTick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(719, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // canvas
+            // 
+            this.canvas.Controls.Add(this.label1);
+            this.canvas.Location = new System.Drawing.Point(-7, -31);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(781, 487);
+            this.canvas.TabIndex = 1;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.UpdateCanvas);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Red;
+            this.canvas.ResumeLayout(false);
+            this.canvas.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel canvas;
+    }
+}
+
---
+++ WindowKill/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//using System.Drawing;
+using System.Runtime.InteropServices;
+//using System.Windows.Forms;
+
+namespace WindowKill
+{
+    public partial class Form1 : Form
+    {
+        private Point circleCenter;
+        private int _circleRadius = 40;
+        private Point shootDirection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            circleCenter = new Point(Size.Width / 2, Size.Height / 2);
+
+            timer.Interval = (int)(1000 / 60.0);
+            timer.Start();
+
+            TopMost = true;
+
+            //canvas.BackColor = Color.Transparent;
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e) { /* Ignore */ }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            //var position = GetCursorPosition();
+
+            //label1.Text = MousePosition.  .ToString();
+
+            shootDirection = new Point(MousePosition.X - Left - circleCenter.X, MousePosition.Y - Top - circleCenter.Y);
+            canvas.Invalidate();
+
+            //if (movingUp && y > 0)
+            //    y -= circleSpeed;
+            //if (movingDown && y < Height - pictureBox1.Height)
+            //    y += circleSpeed;
+            //if (movingLeft && x > 0)
+            //    x -= circleSpeed;
+            //if (movingRight && x < Width - pictureBox1.Width)
+            //    x += circleSpeed;
+
+            //// Update circle position
+            //pictureBox1.Location = new Point(x, y);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            label1.Text = "click";
+            // Shoot if left mouse button is clicked
+            if (e.Button == MouseButtons.Left)
+            {
+                // Add your shooting logic here
+
+                // Get the angle between mouse position and circle position
+                double angle = Math.Atan2(e.Y - (canvas.Location.Y + canvas.Height / 2), e.X - (canvas.Location.X + canvas.Width / 2));
+
+                // Convert angle to degrees
+                double angleInDegrees = angle * (180 / Math.PI);
+
+                // Add your code to shoot using angleInDegrees
+            }
+        }
+
+        private void UpdateCanvas(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            // Draw the circle
+            //g.Clear(Color.White);
+            g.Clear(Color.Red);
+
+            g.FillEllipse(Brushes.Blue, circleCenter.X - _circleRadius, circleCenter.Y - _circleRadius, 2 * _circleRadius, 2 * _circleRadius);
+
+            // Shoot in the mouse direction
+            Pen pen = new Pen(Color.Lime, 3);
+            g.DrawLine(pen, circleCenter, new Point(circleCenter.X + shootDirection.X, circleCenter.Y + shootDirection.Y));
+
+            //var b = new SolidBrush(Color.FromArgb(5, 10, 10, 10));
+            //g.FillRectangle(b, g.ClipBounds);
+        }
+
+        private void CanvasMouseMove(object sender, MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            // Update the shoot direction based on mouse position
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            label1.Text = "move";
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            label1.Text = "nice";
+            circleCenter = new Point(MousePosition.X - Left, MousePosition.Y - Top);
+        }
+    }
+}
